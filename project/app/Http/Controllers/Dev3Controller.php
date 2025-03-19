352e49b013a6797a3780a5d10847c99c2dc90fab
gatamondong@uc-bcf.edu.ph <gatamondong@uc-bcf.edu.ph>
2025-03-19 18:25:42 +0800

    adds dev3 controller

---
+++ project/app/Http/Controllers/Dev3Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Dev3Controller extends Controller
+{
+    public function dev3Controller() {
+        // addtl code
+    }
+}
